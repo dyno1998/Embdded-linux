91fe95a173132390994a6c911b84fdbbe71d0b09
Mohanad Ahmed <87352852+dyno1998@users.noreply.github.com>
2023-08-09 22:43:51 +0300

    task2.py

---
+++ task2.py
@@ -0,0 +1,8 @@
+Task 2 :
+letter=input("Please enter a letter")
+x=letter.lower()
+if x=='a' or x=='e' or x=='i' or x=='o' or x=='u' :
+    print("letter is vowel")
+else :
+    print("letter is not vowel")
+    
