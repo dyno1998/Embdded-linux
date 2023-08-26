2bf71cad2926dda242f558ca677ea40cee4cc6f1
Mohanad Ahmed <87352852+dyno1998@users.noreply.github.com>
2023-08-26 10:04:30 +0300

    Create task10_utlis.py

---
+++ task10_utlis.py
@@ -0,0 +1,11 @@
+import webbrowser as wb
+
+youtube='https://www.youtube.com/'
+linkdin='https://www.linkedin.com/'
+reddit='https://www.reddit.com/'
+
+def func_to_open_url(url):
+  
+  wb.open(f"https://www.{url}.com/")
+
+ 
